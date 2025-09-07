5b515e781dadee96db6d9cf335bdbd613d72a1de
ahmeddin12 <amudi621@gmail.com>
2025-09-07 17:32:38 +0300

    added reviews

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Models\Food\Food;
+use App\Models\Food\Review;
 
 use Illuminate\Http\Request;
 
@@ -11,10 +14,10 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     /**
      * Show the application dashboard.
@@ -23,15 +26,18 @@ class HomeController extends Controller
      */
     public function index()
     {
-     $breakfastFoods = Food::select()->take(4)->
-    where('category', 'Breakfast')->orderBy('id', 'desc')->get(); 
+        $breakfastFoods = Food::select()->take(4)->where('category', 'Breakfast')->orderBy('id', 'desc')->get();
 
-     $lunchFoods = Food::select()->take(4)->
-    where('category', 'Lunch')->orderBy('id', 'desc')->get(); 
+        $lunchFoods = Food::select()->take(4)->where('category', 'Lunch')->orderBy('id', 'desc')->get();
 
-     $dinnerFoods = Food::select()->take(4)->
-       where('category', 'Dinner')->orderBy('id', 'desc')->get(); 
+        $dinnerFoods = Food::select()->take(4)->where('category', 'Dinner')->orderBy('id', 'desc')->get();
 
-       return view('home', compact('breakfastFoods', 'lunchFoods', 'dinnerFoods')); 
+        $reviews = Review::select()->take(4)->get();
+
+        return view('home', compact('breakfastFoods', 'lunchFoods', 'dinnerFoods', 'reviews'));
+    }
+    public function about()
+    {
+        return view('about');
     }
 }
