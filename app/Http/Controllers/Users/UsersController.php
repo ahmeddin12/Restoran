6f11535b2785deb41343cf1d6617cdb6227cce9b
ahmeddin12 <amudi621@gmail.com>
2025-09-07 10:13:41 +0300

    Add UsersController with method to retrieve user bookings

---
+++ app/Http/Controllers/Users/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Users;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Food\Booking;;
+
+use Illuminate\Support\Facades\Auth;
+
+class UsersController extends Controller
+{
+    public function getBookings()
+    {
+        $allBookings = Booking::where("user_id", Auth::user()->id)->get();
+
+        return view('users.bookings', compact('allBookings'));
+    }
+}
