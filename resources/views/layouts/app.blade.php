07a54af5a571c66044a2a7fdbb9fecdf00fb2a40
ahmeddin12 <amudi621@gmail.com>
2025-09-07 10:14:43 +0300

    added drop-down for bookings page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -127,6 +127,9 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('users.bookings') }}">
+                                My Bookings
+                            </a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
