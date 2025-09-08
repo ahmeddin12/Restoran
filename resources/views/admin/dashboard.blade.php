1f72df4ea05dbc6d114b69f676d3b20a11637bf7
ahmeddin12 <amudi621@gmail.com>
2025-09-08 10:54:13 +0300

    created blade template for addmin dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container mt-4"> <!-- Add container and margin-top -->
+  <div class="row g-4"> <!-- g-4 adds gutter spacing between columns -->
+    <div class="col-md-3">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Foods</h5>
+          <p class="card-text">Number of foods: 8</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Orders</h5>
+          <p class="card-text">Number of orders: 4</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Bookings</h5>
+          <p class="card-text">Number of bookings: 4</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Admins</h5>
+          <p class="card-text">Number of admins: 3</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
