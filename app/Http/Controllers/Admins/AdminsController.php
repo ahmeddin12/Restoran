fa9d442c069f00ff6267a7cbe260221e0c9e7f40
ahmeddin12 <amudi621@gmail.com>
2025-09-08 10:52:06 +0300

    added class controller for dashboard

---
+++ app/Http/Controllers/Admins/AdminsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Admins;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+
+
+
+class AdminsController extends Controller
+{
+  public function viewLogin()
+  {
+    return view('admin.login');
+  }
+
+  public function checkLogin(Request $request)
+  {
+
+    $remember_me = $request->has('remember_me') ? true : false;
+
+    if (auth()->guard('admin')->attempt(['email' => $request->input("email"), 'password' => $request->input("password")], $remember_me)) {
+
+      return redirect()->route('admins.dashboard');
+    }
+    return redirect()->back()->with(['error' => 'error logging in']);
+  }
+
+  public function dashboard(Request $request)
+  {
+
+    return view('admin.dashboard');
+  }
+}
