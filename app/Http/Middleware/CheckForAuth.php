af742d4fc91324e4b87e621477e1360be1182fa3
ahmeddin12 <amudi621@gmail.com>
2025-09-11 21:52:20 +0300

    added middleware for the admin

---
+++ app/Http/Middleware/CheckForAuth.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class CheckForAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // If trying to access the login page while already logged in
+
+        if ($request->is('admins/login') && Auth::guard('admin')->check()) {
+            return redirect()->route('admins.dashboard');
+        }
+
+        return $next($request);
+    }
+}
