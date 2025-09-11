ae839f9f1a233905723a082b0f19283f66107657
ahmeddin12 <amudi621@gmail.com>
2025-09-11 21:52:55 +0300

    updated styled for the admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,43 +1,48 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container mt-4" style="margin-top: -150px; margin-left: -200px"> <!-- Add container and margin-top -->
-  <div class="row g-4"> <!-- g-4 adds gutter spacing between columns -->
-    <div class="col-md-3">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Foods</h5>
-          <p class="card-text">Number of foods: 8</p>
-        </div>
+
+<div class="row">
+  <div class="col-md-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Foods</h5>
+        <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
+        <p class="card-text">number of foods: 8</p>
+
       </div>
     </div>
+  </div>
+  <div class="col-md-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Orders</h5>
 
-    <div class="col-md-3">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Orders</h5>
-          <p class="card-text">Number of orders: 4</p>
-        </div>
+        <p class="card-text">number of orders: 4</p>
+
       </div>
     </div>
+  </div>
+  <div class="col-md-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Bookings</h5>
 
-    <div class="col-md-3">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Bookings</h5>
-          <p class="card-text">Number of bookings: 4</p>
-        </div>
+        <p class="card-text">number of bookings: 4</p>
+
       </div>
     </div>
+  </div>
+  <div class="col-md-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Admins</h5>
 
-    <div class="col-md-3">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Admins</h5>
-          <p class="card-text">Number of admins: 3</p>
-        </div>
+        <p class="card-text">number of admins: 3</p>
+
       </div>
     </div>
   </div>
 </div>
+
 @endsection
