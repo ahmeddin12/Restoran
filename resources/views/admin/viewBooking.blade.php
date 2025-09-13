c146cce3d7a8c0b6e2cdb89e94ea00058ad34026
ahmeddin12 <amudi621@gmail.com>
2025-09-13 09:54:34 +0300

    added blade view for displaying all the bookings

---
+++ resources/views/admin/viewBooking.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.admin')
+@section('content')
+
+
+<div class="row">
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title mb-4 d-inline">Bookings</h5>
+
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Booking date</th>
+              <th scope="col">Number of people</th>
+              <th scope="col">Special request</th>
+              <th scope="col">Status</th>
+              <th scope="col">Change Statust</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($bookings as $booking)
+            <tr>
+              <th scope="row">1</th>
+              <td>{{$booking->name}}</td>
+              <td>{{$booking->email}}</td>
+              <td>{{$booking->date}}</td>
+              <td>{{$booking->num_people}}</td>
+              <td>{{$booking->spe_request}}</td>
+              <td>{{$booking->status}}</td>
+              <td><a href="3" class="btn btn-warning text-white  text-center ">Change Status</a></td>
+              <td><a href="delete-bookings.html" class="btn btn-danger  text-center ">delete</a></td>
+            </tr>
+          </tbody>
+          @endforeach
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
