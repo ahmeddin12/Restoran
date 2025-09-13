d391746445c537d491ffa22826520c06e4dd34ea
ahmeddin12 <amudi621@gmail.com>
2025-09-13 09:53:55 +0300

    added middleware -if admin is already logged in, redirect to dashboard page instead of showing the login page

--- app/Http/Middleware/CheckForAuth.php
+++ app/Http/Middleware/CheckForAuth.php
@@ -4,20 +4,13 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 
 class CheckForAuth
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
-        // If trying to access the login page while already logged in
-
+        // If already logged in as admin, redirect to dashboard
         if ($request->is('admins/login') && Auth::guard('admin')->check()) {
             return redirect()->route('admins.dashboard');
         }
