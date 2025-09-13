f73d9868b76a90369fbf0b8dcdc547a748d57f57
ahmeddin12 <amudi621@gmail.com>
2025-09-13 09:52:33 +0300

    middlewares registered

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,25 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Configuration\Exceptions;
+use App\Http\Middleware\CheckForAuth;
+use App\Http\Middleware\AdminAuthenticate;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withMiddleware(function (Middleware $middleware): void {
+        // Register your middleware aliases
+        $middleware->alias([
+            'check.admin.auth' => CheckForAuth::class,
+            'admin.auth'       => AdminAuthenticate::class,
+        ]);
+    })
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
