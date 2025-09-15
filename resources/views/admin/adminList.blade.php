dd487369e6109c06cab30176d9460e1632a3cba7
ahmeddin12 <amudi621@gmail.com>
2025-09-15 22:12:57 +0300

    fixed styling issues

--- resources/views/admin/adminList.blade.php
+++ resources/views/admin/adminList.blade.php
@@ -6,7 +6,6 @@
   <div class="col">
     <div class="card">
       <div class="card-body">
-        <h5 class="card-title mb-4 d-inline">Admins</h5>
         @if (\Session::has('success'))
         <div class="alert alert-success">
           <ul>
@@ -14,6 +13,7 @@
           </ul>
         </div>
         @endif
+        <h5 class="card-title mb-4 d-inline">Admins</h5>
         <a href="{{route('admins.create')}}" class="btn btn-primary mb-4 text-center float-right">Create Admins</a>
         <table class="table">
           <thead>
