85dca3e39baa138dc3461e9f8c558466bdb72bd8
ahmeddin12 <amudi621@gmail.com>
2025-11-03 09:37:28 +0300

    Update breadcrumb links in cart and contact pages for improved navigation

--- resources/views/foods/cart.blade.php
+++ resources/views/foods/cart.blade.php
@@ -5,8 +5,8 @@
         <h1 class="display-3 text-white mb-3 animated slideInDown">Cart</h1>
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb justify-content-center text-uppercase">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item"><a href="#">Cart</a></li>
+                <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+                <li class="breadcrumb-item"><a href="{{route('food.displayCart')}}">Cart</a></li>
             </ol>
         </nav>
     </div>
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -18,87 +18,44 @@
 
 
 <!-- Service Start -->
-<div class="container-xxl py-5">
-  <div class="container">
-    <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-      <h5 class="section-title ff-secondary text-center text-primary fw-normal">Our Services</h5>
-      <h1 class="mb-5">Explore Our Services</h1>
+<div class="container">
+  <div class="row g-4">
+    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+      <div class="service-item rounded pt-3">
+        <div class="p-4">
+          <i class="fa fa-3x fa-user-tie text-primary mb-4"></i>
+          <h5>Master Chefs</h5>
+          <p>Our team of 50+ professional chefs brings creativity and skill to every dish, ensuring top-notch culinary experiences.</p>
+        </div>
+      </div>
     </div>
-    <div class="row g-4">
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="service-item rounded pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-user-tie text-primary mb-4"></i>
-            <h5>Master Chefs</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
+    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+      <div class="service-item rounded pt-3">
+        <div class="p-4">
+          <i class="fa fa-3x fa-utensils text-primary mb-4"></i>
+          <h5>Quality Food</h5>
+          <p>We use only the freshest, locally-sourced ingredients to deliver authentic flavors and wholesome meals.</p>
         </div>
       </div>
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
-        <div class="service-item rounded pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-utensils text-primary mb-4"></i>
-            <h5>Quality Food</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
+    </div>
+    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+      <div class="service-item rounded pt-3">
+        <div class="p-4">
+          <i class="fa fa-3x fa-utensils text-primary mb-4"></i>
+          <h5>Online Order</h5>
+          <p>Convenient online ordering with fast delivery ensures you enjoy your favorite meals anytime, anywhere.</p>
         </div>
       </div>
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-        <div class="service-item rounded pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-cart-plus text-primary mb-4"></i>
-            <h5>Online Order</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
-        <div class="service-item rounded pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-headset text-primary mb-4"></i>
-            <h5>24/7 Service</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="service-item rounded pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-user-tie text-primary mb-4"></i>
-            <h5>Master Chefs</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
-        <div class="service-item rounded pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-utensils text-primary mb-4"></i>
-            <h5>Quality Food</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-        <div class="service-item rounded pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-cart-plus text-primary mb-4"></i>
-            <h5>Online Order</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
-        <div class="service-item rounded pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-headset text-primary mb-4"></i>
-            <h5>24/7 Service</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
+    </div>
+    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+      <div class="service-item rounded pt-3">
+        <div class="p-4">
+          <i class="fa fa-3x fa-utensils text-primary mb-4"></i>
+          <h5>24/7 Service</h5>
+          <p>Our friendly customer service team is available around the clock to assist with reservations and inquiries.</p>
         </div>
       </div>
     </div>
   </div>
 </div>
 <!-- Service End -->
-@endsections
